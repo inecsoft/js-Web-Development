24c800c5f0ba7e1a4ae7fbefcad0d332cc0a251a
ivan-arteaga-tfgm <ivan.arteaga@tfgm.com>
2022-11-28 18:27:38 +0000

    added update

--- sanbox.js
+++ sanbox.js
@@ -1,38 +1,23 @@
-const resource = '';
 
-const getTodos = (resource) => {
-    // const resource = 'https://jsonplaceholder.typicode.com/todos';
-    
-    return new Promise((resolve, reject) => {
-        const req = new XMLHttpRequest();
 
-        req.addEventListener('readystatechange', ()  =>  {
-            if ( req.readyState === 4 && req.status === 200 ){
-                const data = JSON.parse(req.responseText);
-                resolve(data);
-            } else if (req.readyState === 4) {
-                reject('error getting resource');
-            }
-        });
-    
-        req.open('GET', resource);
-        req.send();
+const getTodos = async () => {
+  
+    const resp = await fetch('todos/Ivan.json');
+    const data = await resp.json();
+    console.log(data)
 
-    });
-};
+}
 
-getTodos('todos/Ivan.json').then(data => {
-    console.log('promise resolved Ivan:', data);
-    return getTodos('todos/shaun.json');
-}).then(data => {
-    console.log( 'promise resolved shaun:', data);
-    return getTodos('todos/Mario.json');
-}).then(data => {
-    console.log('promise resolved Mario:', data);
-    return getTodos('todos/Luigi.json');
-}).then(data => {
-    console.log('promise resolved Luigi:', data);
-}).catch(err => {
-    console.log('promise rejected:', err);
-});
+getTodos();
+
+
+
+// fetch('todos/Ivan.json').then(response => {
+//     console.log('resolved:', response);
+//     return response.json();
+// }).then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log('rejected:', err);
+// });
 
