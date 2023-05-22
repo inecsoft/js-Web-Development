f2bb490f9a1a967de2d9228ef7cd697eda41ea33
ivan-arteaga-tfgm <ivan.arteaga@tfgm.com>
2023-05-22 09:59:33 +0000

    freecodecamp

--- next-tailwind-amazona/pages/index.js
+++ next-tailwind-amazona/pages/index.js
@@ -4,20 +4,9 @@ import { Bucket } from 'sst/node/bucket';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import React from 'react';
-import Layout from '../components/Layout';
-import ProductItem from '../components/ProductItem';
-import data from '../utils/data';
 
 export default function Home() {
-  return (
-    <Layout title='Home Page'>
-      <div className='{styles.home}'>
-        {data.products.map((product) => (
-          <ProductItem product={product} key={product.slug}></ProductItem>
-        ))}
-      </div>
-    </Layout>
-  );
+  return <div>Home</div>;
 }
 
 export async function getServerSideProps() {
